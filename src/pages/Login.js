467f94e098e690ce42756aa11e734eea00be8880
arunpratapsingh17 <218apsj@gmail.com>
2020-05-20 23:41:30 +0530

    Login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,76 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
+//strapi function
+
+//handle user
 
 export default function Login() {
-  return <h1>hello from login page</h1>;
+  const history = useHistory();
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [username, setUsername] = React.useState("default");
+  const [isMember, setIsMember] = React.useState(true);
+
+  let isEmpty = false;
+  const toggleMember = () => {};
+  const handleSubmit = async (e) => {};
+  return (
+    <section className="form section">
+      <h2 className="section-title">{isMember ? "sign in" : "Register"}</h2>
+      <form className="login-form">
+        {/* Single Input */}
+        <div className="form-control">
+          <label htmlFor="email">email</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="form-control">
+          <label htmlFor="Password">Password</label>
+          <input
+            type="Password"
+            id="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        {!isMember && (
+          <div className="form-control">
+            <label htmlFor="Username">Username</label>
+            <input
+              type="Username"
+              id="Username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+        )}
+        {/* End of single input */}
+        {/* Empty form text */}
+        {isEmpty && (
+          <p className="form-empty">Please fill out all form fields</p>
+        )}
+        {/* Button for login */}
+        {!isEmpty && (
+          <button
+            className="btn btn-block"
+            type="submit"
+            onClick={handleSubmit}
+          >
+            submit
+          </button>
+        )}
+        {/* Register link */}
+        <p className="register-link">
+          {isMember ? "Need To Register" : "Already a member"}
+          <button type="button" onClick={handleSubmit}>
+            click here
+          </button>
+        </p>
+      </form>
+    </section>
+  );
 }
